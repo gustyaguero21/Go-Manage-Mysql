6a362debb721d70ba03b5dc71d75fc62ad30285e
Gustavo Aguero Ferrari <gustavo.aguero.dev@gmail.com>
2025-02-26 12:26:13 -0300

    Commit: adding router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"go-manage-mysql/cmd/config"
+	"go-manage-mysql/internal/router"
+	"log"
+)
 
+func main() {
+	router := router.SetupRouter()
+	if err := router.Run(config.Port); err != nil {
+		log.Fatal("error starting server. Error: %w", err)
+	}
 }
--- internal/router/urlmapping.go
+++ internal/router/urlmapping.go
@@ -1,1 +1,18 @@
 package router
+
+import (
+	"go-manage-mysql/cmd/config"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UrlMapping(r *gin.Engine) {
+	api := r.Group(config.BaseURL)
+
+	api.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+}
