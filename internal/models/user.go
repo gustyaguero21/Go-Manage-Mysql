ac48bd484deda17c60556aa275601cfc79bb0b63
Gustavo Aguero Ferrari <gustavo.aguero.dev@gmail.com>
2025-02-27 15:52:17 -0300

    Commit: adding repository

---
+++ internal/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+type User struct {
+	ID       string `gorm:"primaryKey;type:varchar(36);not null;unique" json:"id"`
+	Name     string `gorm:"type:varchar(255);not null" json:"name"`
+	Surname  string `gorm:"type:varchar(255);not null" json:"surname"`
+	Username string `gorm:"type:varchar(255);not null;unique" json:"username"`
+	Phone    string `gorm:"type:varchar(255);not null;unique" json:"phone"`
+	Email    string `gorm:"type:varchar(255);not null;unique" json:"email"`
+	Password string `gorm:"type:varchar(255);not null" json:"password"`
+}
---
+++ internal/repository/user.go
@@ -0,0 +1,80 @@
+package repository
+
+import (
+	"fmt"
+	"go-manage-mysql/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *Repository {
+	return &Repository{DB: db}
+}
+
+func (r *Repository) Exists(username string) error {
+	var user models.User
+	result := r.DB.Where("username=?", username).First(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *Repository) Search(username string) (models.User, error) {
+	var user models.User
+	result := r.DB.Where("username=?", username).First(&user)
+	if result.Error != nil {
+		return models.User{}, result.Error
+	}
+
+	return user, nil
+}
+
+func (r *Repository) Save(user models.User) error {
+	result := r.DB.Create(user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *Repository) Update(username string, update models.User) error {
+	result := r.DB.Where("username=?", username).Select("name", "surname", "phone", "email").Updates(&update)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("no rows affected")
+	}
+	return nil
+}
+
+func (r *Repository) Delete(username string) error {
+	result := r.DB.Model(&models.User{}).Where("username=?", username).Delete(&models.User{})
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("no rows affected")
+	}
+
+	return nil
+}
+
+func (r *Repository) ChangePwd(username string, newPwd string) error {
+	result := r.DB.Model(&models.User{}).Where("username = ?", username).Update("password", newPwd)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("no rows affected")
+	}
+	return nil
+}
