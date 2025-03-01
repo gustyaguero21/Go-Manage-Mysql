333e402b9276835bca4ebe07de914e316e0a9667
Gustavo Aguero Ferrari <gustavo.aguero.dev@gmail.com>
2025-03-01 15:19:28 -0300

    Commit: adding to interface

--- internal/services/services.go
+++ internal/services/services.go
@@ -8,4 +8,7 @@ import (
 type UserServices interface {
 	CreateUser(ctx context.Context, user models.User) (created models.User, err error)
 	SearchUser(ctx context.Context, username string) (user models.User, err error)
+	UpdateUser(ctx context.Context, username string, update models.User) (err error)
+	DeleteUser(ctx context.Context, username string) (err error)
+	ChangeUserPwd(ctx context.Context, username string, newPwd string) (err error)
 }
